a2377ac65a595a93a4548651dca7446530af0b80
Isko2003 <theazeisko2003@gmail.com>
2024-09-12 12:56:28 +0400

    last corrections

--- src/Config/RouterConfig.jsx
+++ src/Config/RouterConfig.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import Home from '../Pages/Home';
-import ProductDetails from '../Components/ProductDetails';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Pages/Home";
+import ProductDetails from "../Components/ProductDetails";
 const RouterConfig = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/product-details/:id' element={<ProductDetails />}/>
-        </Routes>
+      <Routes>
+        <Route path="/e-commerce-vite-react" element={<Home />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default RouterConfig
+export default RouterConfig;
